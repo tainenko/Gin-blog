d81eb53b7c8a74a1c1a76a41945da249d044d9b6
ko tainen <TonyK@hyvesolutions.com>
2020-07-13 00:24:11 +0800

    Add createBlogArticleTable

--- db/schema.go
+++ db/schema.go
@@ -1,13 +1,30 @@
 package db
-func createBlogTagTable(){
+
+func createBlogTagTable() {
 	DB.Query(`
 			CREATE TABLE IF NOT EXISTS blog_tag(
 			id int(10) unsigned NOT NULL AUTO_INCREMENT,
-			name varchar(100) DEFAULT ""
-			created_on int(10) unsigned DEFAULT "0"
-			modified_on int(10) unsigned DEFAULT "0"
-			modified_by varchar(100) DEFAULT ""
-			state tinyint(3) unsigned DEFAULT "1"
+			name varchar(100) DEFAULT "",
+			created_on int(10) unsigned DEFAULT "0",
+			modified_on int(10) unsigned DEFAULT "0",
+			modified_by varchar(100) DEFAULT "",
+			state tinyint(3) unsigned DEFAULT "1",
 			PRIMARY KEY("id")
 		)`)
+}
+
+func createBlogArticleTable() {
+	DB.Query(`
+			id int(10) unsigned NOT NULL AUTO_INCREMENT,
+			tag_id int(10) unsigned DEFAULT "0",
+			title varchar(100) DEFAULT "",
+			desc varc har(255) DEFAULT "",
+			content text,
+			created_on int(11) DEFAULT NULL,
+			created_by varchar(100) DEFAULT""
+			modified_on int(10) unsigned DEFAULT "0",
+			modified_by varchar(255) DEFAULT"",
+			state tinyint(3) unsigned DEFAULT "1",
+			PRIMARY KEY("id")`
+	)
 }
