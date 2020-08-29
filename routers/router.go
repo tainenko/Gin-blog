67d4a21a0c73d9de5f5bfefbfe114636f8f46fa7
ko tainen <TonyK@hyvesolutions.com>
2020-08-29 09:26:55 +0800

    update router.go with apiv1

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"github.com/gin-blog/pkg/setting"
+	"github.com/gin-blog/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,6 +11,12 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
+	apiv1 := r.Group("/api/v1"){
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTags)
+		apiv1.PUT("/tags", v1.PutTags)
+		apiv1.DELETE("/tags", v1.DelTags)
+	}
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "test",
