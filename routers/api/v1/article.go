9be0b6f068f76cb05292f47eac0df34321b87c73
ko tainen <TonyK@hyvesolutions.com>
2020-08-29 22:46:52 +0800

    init article router

---
+++ routers/api/v1/article.go
@@ -0,0 +1,20 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+func GetArticle(c *gin.Context) {
+
+}
+
+func GetArticles(c *gin.Context) {
+
+}
+func AddArticle(c *gin.Context) {
+
+}
+func EditArticle(c *gin.Context) {
+
+}
+func DelArticle(c *gin.Context) {
+
+}
