15abcaeb895412904d6b88ab2910fd614998ebc5
ko tainen <TonyK@hyvesolutions.com>
2020-08-29 22:37:28 +0800

    add EditTag and DelTag api

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -67,12 +67,70 @@ func AddTags(c *gin.Context) {
 
 }
 
-// PutTags 更新指定文章標籤
-func PutTags(c *gin.Context) {
-
+// EditTags 更新指定文章標籤
+func EditTags(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+	name := c.Query("name")
+	modifiedBy := c.Query("modified_by")
+	valid := validation.Validation{}
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		valid.Range(state, 0, 1, "state").Message("狀態只允許0或1")
+	}
+	valid.Required(id, "id").Message("id不能為空")
+	valid.Required(modifiedBy, "modified_by").Message("修改人不能為空")
+	valid.MaxSize(modifiedBy, 100, "modified_by").Message("修改人最長為100個字元")
+	valid.MaxSize(name, 100, "name").Message("名稱最長為100個字元")
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		code = e.SUCCESS
+		if models.ExistTagById(id) {
+			data := make(map[string]interface{})
+			data["modified_by"] = modifiedBy
+			if name != "" {
+				data["name"] = name
+			}
+			if state != -1 {
+				data["state"] = state
+			}
+			models.EditTag(id, data)
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
 
 // DelTags 刪除指定文章標簽
 func DelTags(c *gin.Context) {
-
+	id := com.StrTo(c.Param("id")).MustInt()
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必須大於0")
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		code = e.SUCCESS
+		if models.ExistTagByID(id) {
+			models.DeleteTag(id)
+		} else {
+			code = e.ERROR_NOT_EXIST_TAG
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
