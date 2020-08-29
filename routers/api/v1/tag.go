160fc2779fa49832caa2f66dbbff5f66ef614bd1
ko tainen <TonyK@hyvesolutions.com>
2020-08-29 09:23:59 +0800

    Add tag.go

---
+++ routers/api/v1/tag.go
@@ -0,0 +1,25 @@
+package v1
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// GetTags 獲取標籤列表
+func GetTags(c *gin.Context) {
+
+}
+
+// AddTags 新建文章標籤
+func AddTags(c *gin.Context) {
+
+}
+
+// PutTags 更新指定文章標籤
+func PutTags(c *gin.Context) {
+
+}
+
+// DelTags 刪除指定文章標簽
+func DelTags(c *gin.Context) {
+
+}
