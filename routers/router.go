f59e2b94de1770150ed364ac4bc6e0b47113a596
ko tainen <TonyK@hyvesolutions.com>
2020-08-29 22:50:54 +0800

    update router

--- routers/router.go
+++ routers/router.go
@@ -17,6 +17,12 @@ func InitRouter() *gin.Engine {
 		apiv1.POST("/tags", v1.AddTags)
 		apiv1.PUT("/tags/:id", v1.EditTags)
 		apiv1.DELETE("/tags/:id", v1.DelTags)
+
+		apiv1.GET("/articles", v1.GetArticles)
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		apiv1.POST("/articles", v1.AddArticle)
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, gin.H{
