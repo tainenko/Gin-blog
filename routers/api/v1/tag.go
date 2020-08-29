b7e4655cc28684b01bafa1ecd6a8ea028fad4c14
ko tainen <TonyK@hyvesolutions.com>
2020-08-29 16:24:26 +0800

    update GetTags

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,12 +1,36 @@
 package v1
 
 import (
+	"github.com/gin-blog/models"
+	"github.com/gin-blog/pkg/e"
+	"github.com/gin-blog/pkg/setting"
+	"github.com/gin-blog/util"
 	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+	"net/http"
 )
 
 // GetTags 獲取標籤列表
 func GetTags(c *gin.Context) {
-
+	name := c.Query("name")
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+	if name != "" {
+		maps["name"] = name
+	}
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] = state
+	}
+	code := e.SUCCESS
+	data["lists"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	data["total"] = models.GetTagTotal(maps)
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
 // AddTags 新建文章標籤
