cf075695da4f0f5ec9b234b82635a7693399a557
ko tainen <TonyK@hyvesolutions.com>
2020-08-30 08:44:42 +0800

    add GetArticle

--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -1,9 +1,38 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/astaxie/beego/validation"
+	"github.com/gin-blog/models"
+	"github.com/gin-blog/pkg/e"
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+	"log"
+	"net/http"
+)
 
 func GetArticle(c *gin.Context) {
-
+	id := com.StrTo(c.Param("id")).MustInt()
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必須大於0")
+	code := e.INVALID_PARAMS
+	var data interface{}
+	if !valid.HasErrors() {
+		if models.ExistArticleByID(id) {
+			data = models.GetArticle(id)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Printf(err.Key, err.Message)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
 func GetArticles(c *gin.Context) {
@@ -15,6 +44,6 @@ func AddArticle(c *gin.Context) {
 func EditArticle(c *gin.Context) {
 
 }
-func DelArticle(c *gin.Context) {
+func DeleteArticle(c *gin.Context) {
 
 }
