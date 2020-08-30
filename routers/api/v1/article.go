34e8c922a39358284d30410afb289565ae179025
ko tainen <TonyK@hyvesolutions.com>
2020-08-30 09:15:43 +0800

    add EditArticle

--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -111,7 +111,60 @@ func AddArticle(c *gin.Context) {
 
 }
 func EditArticle(c *gin.Context) {
-
+	valid := validation.Validation{}
+	id := com.StrTo(c.Param("id")).MustInt()
+	tagId := com.StrTo(c.Query("tag_id")).MustInt()
+	title := c.Query("tital")
+	desc := c.Query("desc")
+	content := c.Query("content")
+	modifiedBy := c.Query("modified_by")
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		valid.Range(state, 0, 1, "state").Message("狀態只允許0或1")
+	}
+	valid.Min(id, 1, "id").Message("ID必須大於0")
+	valid.MaxSize(title, 100, "title").Message("標題最長為100個字元")
+	valid.MaxSize(desc, 255, "desc").Message("敘述最長為255個字元")
+	valid.MaxSize(content, 65535, "content").Message("內容最長為65535個字元")
+	valid.Required(modifiedBy, "modified_by").Message("修改人不能為空白")
+	valid.MaxSize(modifiedBy, 100, "modified_by").Message("修改人最長為100個字元")
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if models.ExistArticleByID(id) {
+			if models.ExistTagByID(tagId) {
+				data := make(map[string]interface)
+				if tagId > 0 {
+					data["tag_id"] = tagId
+				}
+				if title != "" {
+					data["title"] = title
+				}
+				if desc != "" {
+					data["desc"] = desc
+				}
+				if content != "" {
+					data["content"] = content
+				}
+				data["modified_by"] = modifiedBy
+				models.EditArticle(id, data)
+				code = e.SUCCESS
+			} else {
+				code = e.ERROR_NOT_EXIST_TAG
+			}
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
 func DeleteArticle(c *gin.Context) {
 
