f350b7ec40249eeacf3b06b5a35c7a6acb6763af
ko tainen <TonyK@hyvesolutions.com>
2020-08-30 09:19:35 +0800

    add DeleteArticle

--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -167,5 +167,26 @@ func EditArticle(c *gin.Context) {
 	})
 }
 func DeleteArticle(c *gin.Context) {
+	id := com.StrTo(c.Param("id")).MustInt()
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必須大於0")
+	code := e.INVALID_PARAMS
+	if !valid.HasErrors() {
+		if models.ExistArticleByID(id) {
+			models.DeleteArticle(id)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		for _, err := range valid.Errors {
+			log.Println(err.Key, err.Message)
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 
 }
